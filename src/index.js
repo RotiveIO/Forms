e7643c3e2693343e8b14951bf7bb9e595544ac9d
Robert Lester <rlester@roberts-macbook-pro.local>
2021-05-21 20:57:40 +0000

    ..

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import './sass/_index.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+$('.text,.select,textarea').addClass("form-control");
+$('.pd-checkbox input').addClass('form-check-input');
+$('.pd-checkbox label').addClass('form-check-label');
+$('.pd-checkbox span.value').addClass('form-check');
+
+$('label').each(function() {
+	var placeHolder = $(this).text();
+	var forId = $(this).attr('for');
+	$('#' + forId).attr('placeholder', placeHolder);
+});
