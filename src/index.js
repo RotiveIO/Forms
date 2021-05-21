2d84d52d9a74377fb28cf09da07fffc63e35ea05
Robert Lester <rlester@roberts-macbook-pro.local>
2021-05-21 19:58:24 +0000

    ..

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './sass/_index.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,22 @@ module.exports = {
 						loader: 'sass-loader' // compiles Sass to CSS
 					}
 				]
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+				  {
+					loader: 'file-loader',
+					options: {
+					  name: '[name].[ext]',
+					  outputPath: 'fonts/'
+					}
+				  }
+				]
 			}
 		]
 	}
